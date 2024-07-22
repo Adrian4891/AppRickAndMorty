4830dcdbb4e627a378eac5ec312e28b64df490cc
AdrianEr <105084571+Adrian4891@users.noreply.github.com>
2024-07-22 13:35:21 -0300

    Update DB_connection.js

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -4,9 +4,16 @@ const { Sequelize } = require("sequelize");
 const characterModel = require("./models/Character");
 const favoriteModel = require("./models/Favorite");
 
-const sequelize = new Sequelize(DB_URL,
-   {logging: false, native: false}
-);
+const sequelize = new Sequelize(DB_URL,{
+   logging: false, 
+   native: false
+   dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Ajusta esto según tu configuración de certificados
+      }
+    }
+});
 
 characterModel(sequelize);
 favoriteModel(sequelize);
